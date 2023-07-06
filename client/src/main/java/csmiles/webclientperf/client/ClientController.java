19bc951a8f087018763d0ccd1a61cda0c0f8cf6c
Craig Smiles <smiley10@gmail.com>
2023-07-06 09:50:44 +0100

    Use K8s envvars for URL request in ClientController

--- client/src/main/java/csmiles/webclientperf/client/ClientController.java
+++ client/src/main/java/csmiles/webclientperf/client/ClientController.java
@@ -1,25 +1,34 @@
 package csmiles.webclientperf.client;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import java.net.URI;
+
 @RestController
 @RequestMapping("client")
 public class ClientController {
 
+    private final URI serverScheme;
+
     private final WebClient webClient;
 
-    public ClientController(WebClient webClient) {
+    public ClientController(
+            @Value("http://${SERVER_SVC_SERVICE_HOST}:${SERVER_SVC_SERVICE_PORT}") URI serverScheme,
+            WebClient webClient
+    ) {
+        this.serverScheme = serverScheme;
         this.webClient = webClient;
     }
 
     @GetMapping("retrieveSuccess")
     public Mono<String> retrieveSuccess() {
         return webClient.get()
-                .uri("http://localhost:8080/server/success")
+                .uri(serverScheme + "/server/success")
                 .retrieve()
                 .bodyToMono(String.class);
     }
